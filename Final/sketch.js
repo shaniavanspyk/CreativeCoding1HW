bf4f81a5236ec7e766a999f9ea6d78aa8164e77c
shaniavanspyk <shania.vanspyk@umconnect.umt.edu>
2024-05-02 11:59:14 -0600

    final

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,42 +1,68 @@
-/*
------ Coding Tutorial by Patt Vira ----- 
-Name: Interactive Floating Typography
-Video Tutorial: https://youtu.be/-6v_AYyn49k
+//when mouse moves to the left - rotate counterclockwise 
+//when mouse moves to the right - rotate clockwise
 
-Connect with Patt: @pattvira
-https://www.pattvira.com/
-----------------------------------------
-*/
-
-
-let fonts = [];
-let letters = [];
-
-function preload() {
-  fonts[0] = loadFont("fonts/Roboto-Regular.ttf");
-  fonts[1] = loadFont("fonts/Kanit-Black.ttf");
-  fonts[2] = loadFont("fonts/BebasNeue-Regular.ttf");
-}
-
+let gap = 10;
+let cirNum = 75;
+let cirSize = 20;
+let angle= 0;
+let ptNum = 50;
+let rectSize = 1020;
 function setup() {
-  createCanvas(400, 400);
-  angleMode(DEGREES);
+  createCanvas(windowWidth,windowHeight);
+  angleMode(degrees);
   
 }
 
 function draw() {
-  background("#FF0F8D");
-  for (let i=letters.length-1; i >= 0; i--) {
-    letters[i].update();
-    letters[i].display();
-    
-    if (letters[i].offScreen() == true) {
-      letters.splice(i, 1);
-    }
-  }
+  background("#376204");
+  //customized shape for mouse
+  noCursor();
+  noStroke();
+  fill("#1C3103")
+  circle(mouseX,mouseY,10);
   
+  //main graph
+push();
+translate(width/2,height/2);
+rotate(angle);
+angle = angle+map(mouseX,0,width,-0.1,0.1);
+noFill();
+stroke("white")
+strokeWeight(1)
+for (let i = 0; i < cirNum; i++){
+  arc(0, 0, cirSize + gap * i,cirSize + gap * i,angle*i,360-angle/2);
 }
-
-function mouseDragged() {
-  letters.push(new Letter(mouseX, mouseY));
+  pop();
+  
+  
+  //title
+  push();
+  translate(width/2,height-90);
+  noStroke();
+  textFont('Arial');
+  textSize(30);
+  textAlign(CENTER,CENTER);
+  text('RADAR',0,0);
+  noStroke();
+  textSize(15)
+  text('Final by:Shania Van Spyk',0,30);
+  pop();
+  
+  //border
+  push();
+  translate(width/2,height/2);
+  noFill();
+  stroke('white');
+  strokeWeight(2);
+  rectMode(CENTER);
+  rect(0,0,rectSize,rectSize);
+  
+   //background noise
+  push();
+  stroke("white");
+  strokeWeight(1.5);
+  for(let i=0; i<ptNum; i++){
+    point(random(-rectSize/2,rectSize/2),random(-rectSize/2,rectSize/2));
+  }
+  pop();
 }
